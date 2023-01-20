c9db4f4fa206b368572e7b06009cbe00613e738b
ViniciusRoosevelt <vinicius.roosevelt@paraiba.email>
2023-01-20 09:26:22 -0300

    Modifie Model

--- django-rest/escola/admin.py
+++ django-rest/escola/admin.py
@@ -6,7 +6,7 @@ from escola.models import Aluno, Curso
 class Alunos(admin.ModelAdmin):
     list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
     list_display_links = ('id', 'nome')
-    search_fields = ('nome')
+    search_fields = ['nome']
     list_per_page = 20
 
 
@@ -16,8 +16,9 @@ admin.site.register(Aluno, Alunos)
 class Cursos(admin.ModelAdmin):
     list_display = ('id', 'código_curos', 'descrição', 'nível')
     list_display_links = ('id', 'código_curos', 'descrição')
-    search_fields = ('código_curos', 'descrição')
+    search_fields = ['código_curos']
     list_per_page = 10
 
 
 admin.site.register(Curso, Cursos)
+ 
