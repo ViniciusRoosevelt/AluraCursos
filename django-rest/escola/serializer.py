b901e41926c39f5a3733200ec095b7a6cd8c555d
ViniciusRoosevelt <vinicius.roosevelt@paraiba.email>
2023-01-20 09:39:15 -0300

    Add Curso and Aluno Serializer

--- django-rest/escola/serializer.py
+++ django-rest/escola/serializer.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from escola.models import Aluno, Curso
 
 
-class AlunosSerializer(serializers.ModelSerializer):
+class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'name', 'rg', 'cpf', 'data_nascimento']
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = ['id', 'descrição', 'nível']
