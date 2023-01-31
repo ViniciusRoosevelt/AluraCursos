a1f84a65a46aaa4d28b9548d440b8584858606c9
ViniciusRoosevelt <vinicius.roosevelt@paraiba.email>
2023-01-31 10:05:19 -0300

    Initial Front-End

--- crud/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-class ImagemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'creator', 'creator_id', 'title', 'description', 'image_url',)
-    list_display_links = ('id', 'creator', 'creator_id', 'title', 'description', 'image_url',)
-    search_fields = ('title','description')
-    list_per_page = 10
-admin.site.register(Imagem,ImagemAdmin)
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Log-In</h1>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <button>Entrar</button>
+      </form>
+    </>
+  );
+}
